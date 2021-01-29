f5738cddffc4fda7403325df325e47a011d4ec0c
Benjamin <ben.scialom@gmail.com>
2021-01-29 20:29:54 +0100

    Create Library
    set up the library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='BenUpFin',
+    packages=find_packages(include=['BenUpFin']),
+    version='0.1.0',
+    description='Pyhton library related to finance: risk , portfolio management etc..',
+    author='Benjamin Scialom',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
